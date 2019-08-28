09a3165ca7f107529de336e4a4d6760407121b7f
Chris Anderson <andersan@umich.edu>
2019-08-28 05:09:03 -0500

    Add loop to program and refactor

--- app/conjugate_test.py
+++ app/conjugate_test.py
@@ -3,16 +3,21 @@ from pattern.fr import verbs
 import random
 
 some_verbs = list(verbs.infinitives.keys())
-print(some_verbs)
 possible_persons = [1, 2, 3]
 possible_plurality = ["sg", "pl"]
-verb = random.choice(some_verbs)
-pp = str(random.choice(possible_persons)) + random.choice(possible_plurality)
-result = conjugate(verb, pp)
-print("\n")
-attempt = input("Conjugate " + verb + " in " + pp + ": \n")
 
-if (attempt == result):
-	print("Correct")
-else:
-	print("Incorrect - should be " + result) 
+def get_verb():
+	verb = random.choice(some_verbs)
+	pp = str(random.choice(possible_persons)) + random.choice(possible_plurality)
+	return conjugate(verb, pp), verb, pp
+
+
+if __name__== "__main__":
+	while True:
+		result, verb, pp = get_verb()
+		attempt = input("Conjugate " + verb + " in " + pp + ": \n")
+
+		if (attempt == result):
+			print("Correct")
+		else:
+			print("Incorrect - should be " + result)
